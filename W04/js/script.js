a95926a8e9a45d16bb5984a5bf658b5ed10213e8
Shayla Nelson <xelamarie@gmail.com>
2021-05-12 16:31:22 -0600

    preston page

---
+++ W04/js/script.js
@@ -0,0 +1,11 @@
+document.querySelector(".current-year").innerHTML = new Date().getFullYear();
+document.querySelector(".today-date").innerHTML = new Intl.DateTimeFormat("en-US", {
+   weekday: "long",
+   year: "numeric",
+   month: "long",
+   day: "numeric",
+}).format(new Date());
+
+function toggleMenu () {
+   document.getElementById("primaryNav").classList.toggle("hide");
+}
