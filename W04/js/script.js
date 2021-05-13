50a8bf4804e2214e694552eee3dbed0501c77c03
Shayla Nelson <xelamarie@gmail.com>
2021-05-12 22:48:20 -0600

    move onclick

--- W04/js/script.js
+++ W04/js/script.js
@@ -1,11 +1,14 @@
 document.querySelector(".current-year").innerHTML = new Date().getFullYear();
-document.querySelector(".today-date").innerHTML = new Intl.DateTimeFormat("en-GB", {
-   weekday: "long",
-   year: "numeric",
-   month: "long",
-   day: "numeric",
-}).format(new Date());
+document.querySelector(".today-date").innerHTML = new Intl.DateTimeFormat(
+  "en-GB",
+  {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+).format(new Date());
 
-function toggleMenu() {
-   document.getElementById("primaryNav").classList.toggle("hide");
-}
+document.querySelector(".menu-button").addEventListener("click", () => {
+  document.querySelector(".primary-nav").classList.toggle("hide");
+});
