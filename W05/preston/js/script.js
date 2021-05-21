f4b3d0b1374840e228623e573a3e13cda6835f6c
Shayla Nelson <xelamarie@gmail.com>
2021-05-20 23:39:53 -0600

    viewport fixes and friday banner

--- W05/preston/js/script.js
+++ W05/preston/js/script.js
@@ -1,14 +1,22 @@
+// Display today's date in footer
 document.querySelector(".current-year").innerHTML = new Date().getFullYear();
 document.querySelector(".today-date").innerHTML = new Intl.DateTimeFormat(
-  "en-GB",
-  {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  }
+   "en-GB", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+   }
 ).format(new Date());
 
+
+// Toggle hamburger menu on click 
 document.querySelector(".menu-button").addEventListener("click", () => {
-  document.querySelector(".primary-nav").classList.toggle("hide");
+   document.querySelector(".primary-nav").classList.toggle("hide");
 });
+
+// Display Pancake Breakfast banner on Fridays
+const dayOfWeek = new Date().getDay(); // Sunday is 0
+if (dayOfWeek === 5) {
+   document.querySelector(".banner").style.display = "block";
+}
