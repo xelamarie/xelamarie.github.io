1e35e008bb8a8a32a9a6bac5c9b7411e5e72665f
Shayla Nelson <xelamarie@gmail.com>
2021-05-26 12:45:09 -0600

    fix images

--- W06/js/windchill.js
+++ W06/js/windchill.js
@@ -4,10 +4,10 @@ var s = parseFloat(document.querySelector(".wind-speed").textContent);
 
 function windChill(fahrTemp, windSpeed) {
    if (fahrTemp <= 50 && windSpeed > 3) {
-      document.querySelector(".wind-chill").innerHTML = Math.round(35.74 + (0.6215 * fahrTemp) - (35.75 * windSpeed ** 0.16) + (0.4275 * fahrTemp * windSpeed ** 0.16));
+      return Math.round(35.74 + (0.6215 * fahrTemp) - (35.75 * windSpeed ** 0.16) + (0.4275 * fahrTemp * windSpeed ** 0.16));
    } else {
-      document.querySelector(".wind-chill").innerHTML = "N/A";
+      return "N/A";
    }
 }
 
-windChill(t, s);
+document.querySelector(".wind-chill").innerHTML = windChill(t, s);
