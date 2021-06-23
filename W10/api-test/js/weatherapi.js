562575117d8a266c63bd215f131fcda7e6841ee4
Shayla Nelson <xelamarie@gmail.com>
2021-06-23 00:29:28 -0600

    preston weather api

---
+++ W10/api-test/js/weatherapi.js
@@ -0,0 +1,13 @@
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=177fcadbd61ccd88c744e5b81edf9e26';
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    document.getElementById('current-temp').textContent = jsObject.main.temp;
+    const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
+    const desc = jsObject.weather[0].description;  // note how we reference the weather array
+    document.getElementById('imagesrc').textContent = imagesrc;  // informational specification only
+    document.getElementById('icon').setAttribute('src', imagesrc);  // focus on the setAttribute() method
+    document.getElementById('icon').setAttribute('alt', desc);
+  });
+
---
+++ W10/js/windchill.js
@@ -0,0 +1,11 @@
+// Calculate wind chill
+var t = parseFloat(document.getElementById('current-temp').textContent);
+var s = parseFloat(document.getElementById('wind-speed').textContent);
+
+function windChill(fahrTemp, windSpeed) {
+   if (fahrTemp <= 50 && windSpeed > 3) {
+      return Math.round(35.74 + (0.6215 * fahrTemp) - (35.75 * windSpeed ** 0.16) + (0.4275 * fahrTemp * windSpeed ** 0.16));
+   } else {
+      return null;
+   }
+}
